f5ea893d9748ff2d2bc6dbfcbc80a53ad5570e91
Emulie Chhor <emulie.chhor@hotmail.com>
2021-07-15 10:25:17 -0400

    fixed #3: saving status as csv file after n songs

--- Downloader.py
+++ Downloader.py
@@ -1,4 +1,4 @@
-# /usr/bin/env
+#!/usr/bin/env
 
 """ Class which download songs/books not in a repository from a url """
 
@@ -26,6 +26,8 @@ class Downloader:
             }],
             'keepvideo': False
         }
+        self.autosave = True  # update csv file every n songs
+        self.n = 10  # save file to csv every n songs
 
     def read_csv_file(self):  # refractor in CSVManager?
         """docstring for read_csv_file"""
@@ -34,6 +36,9 @@ class Downloader:
 
     def download_songs(self):
         """ Download songs if it hasn't been downloaded before """
+        # verify if songs have been downloaded
+
+        # download missing songs
         for index, row in self.df.iterrows():
             if row["isDownloaded"] == False:
                 print("Downloading " + row["artist"] + " " + row["title"])
@@ -45,17 +50,28 @@ class Downloader:
                 with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
                     #  download songs
                     song_url = row["url"]
-                    ydl.download([song_url])
-                    self.df.loc[index, "isDownloaded"] = True
+                    try:
+                        ydl.download([song_url])
+                        self.df.loc[index, "isDownloaded"] = True
+                    except:  # download has failed
+                        print(
+                            "Download of: " + row["artist"] + " - " + row["title"] + " has failed")
+                        self.save_df_to_csv()
+
+            # update csv file every n songs
+            if self.autosave and index % self.n == 0:
+                print("AUTOSAVING...")
+                self.save_df_to_csv()
+
         #  save updated csv file
         self.save_df_to_csv()
 
     def save_df_to_csv(self):  # refractor to csv Manager?
-        new_file_path = self.csv_path.split(".csv")[0] + '_new' + '.csv'
-        self.df.to_csv(new_file_path, sep=',', index=False)
+        #  new_file_path = self.csv_path.split(".csv")[0] + '_new' + '.csv'
+        self.df.to_csv(self.csv_path, sep=',', index=False)
 
 
 if __name__ == "__main__":
     downloader = Downloader(downloadPath="C:/Users/emuli/Downloads/",
-                            csv_path="Playlist/test.csv")
+                            csv_path="Playlist/Emo kid_changes.csv")
     downloader.download_songs()
