250794361de6f5b067006d1c33c0213c5392d61d
Emulie Chhor <emulie.chhor@hotmail.com>
2021-07-14 23:06:06 -0400

    added generateDataFrameFromYoutubeMusicPlaylist(): generate data frame with [artist, title, album, isDownloaded]

--- GeneratorCSV.py
+++ GeneratorCSV.py
@@ -1,35 +1,68 @@
 #!/usr/bin/env
 
-""" Class that generate csv file from playlist url """
+""" Class that generate csv file from playlist playlist_id """
 
+import json
+import pandas as pd
 import requests
+from ytmusicapi import YTMusic
 
 
 class GeneratorCSV:
-    def __init__(self, url):
+    def __init__(self, playlist_id):
         """
-        url: playlist of url
+        playlist_id: playlist of url
         """
-        self.url = url
+        self.playlist_id = playlist_id
+        self.df = self.generateDataFrameFromYoutubeMusicPlaylist()
 
-    def generateListFromYoutubeMusic(self):
+    def generateDataFrameFromYoutubeMusicPlaylist(self):
         """ Generate List of Songs from Youtube Playlist """
-        response = requests.get(self.url)
-        if response.status_code == 200:  # successfully reached webpage
-            print("Request succesful")
-        else:
-            print("Request failed")
+
+        # API request for Youtube Music
+        ytmusic = YTMusic()
+        playlist = ytmusic.get_playlist(
+            playlistId=self.playlist_id, limit=500)
+        # json file of tracks: https://ytmusicapi.readthedocs.io/en/latest/reference.html
+        tracks = playlist["tracks"]
+        #  print(playlist["id"])
+
+        # Generate all the rows: we want to generate data and then create data frame because it is faster: https://stackoverflow.com/questions/13784192/creating-an-empty-pandas-dataframe-then-filling-it
+        data = []
+        for i, track in enumerate(tracks):
+            # Get Meta Data
+            artist = track["artists"][0]["name"]
+            title = track["title"]
+            if track["album"] != None:
+                album = track["album"]["name"]
+            else:
+                album = None
+            #  duration = track["duration"] # TO FIX: duration has bug
+            video_id = track["videoId"]
+            isDownloaded = False  # TODO: check if song is already downloaded
+            # append row to dataframe
+            if video_id != None:  # sinon, l'url n'est pas valide
+                url = "https://music.youtube.com/watch?v=" + \
+                    video_id + "list=" + self.playlist_id
+                row = [artist, title, album, url, isDownloaded]
+                data.append(row)
+            #  df.loc[i] = row  # DONT APPEND DIRECTLY: Too slow
+
+        # generate dataframe
+        COLUMN_NAMES = ['artist', 'title', 'album', 'url', 'isDownloaded']
+        df = pd.DataFrame(data, columns=COLUMN_NAMES)
+        return df
 
     def generateListFromSpotify(self):
         """ Generate List of Songs from Spotify Playlist """
         pass
 
-    def generateListFromGoodreads(self):
-        """ Generate List of Songs from Youtube Playlist """
+    def find_song_url(self):
+        """ Find song playlist_id from youtube with key: artist+title+lyrics """
         pass
 
 
 if __name__ == "__main__":
-    url = "https://music.youtube.com/playlist?list=PLzx7xtGqjNzoahrq-AQmO7DHbJZtGqZUC"
-    generator = Generator(url)
-    generator.generateListFromYoutubeMusic()
+    playlist_id = "PLzx7xtGqjNzoahrq-AQmO7DHbJZtGqZUC"
+    generator = GeneratorCSV(playlist_id)
+    #  generator.generateDataFrameFromYoutubeMusicPlaylist()
